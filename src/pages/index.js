da5c8f099314e441e8157231a4a24a9cb58eae75
Veer <veer.bhatia@mail.mcgill.ca>
2020-07-03 12:12:44 -0700

    added subtext

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Meet Orwell, the world's first social media platform.</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Take back your time today.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
